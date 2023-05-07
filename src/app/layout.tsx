26b36f5a3bf94ded00d164703061b760210fb2e9
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-07 13:15:36 -0500

    feat: add display swap to font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Poppins } from 'next/font/google'
 import './globals.css'
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '700'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '700'], display: 'swap' })
 
 export const metadata = {
   title: 'Anime Quiz',
